0401318146d1e48c3b06914e02a28aeccf54efd4
Ian Korf <ifkorf@gmail.com>
2022-02-16 07:26:19 -0800

    in

--- pubquarterly.py
+++ pubquarterly.py
@@ -3,6 +3,7 @@ import argparse
 import datetime
 import json
 import os
+import sys
 import time
 
 def splitdate(s):
@@ -23,16 +24,16 @@ beg = datetime.date(y1, m1, d1)
 end = datetime.date(y2, m2, d2)
 delta = datetime.timedelta(days=1)
 
-####################
-# Download Section #
-####################
+##############################
+# Scicrunch Download Section #
+##############################
 
 while beg <= end:
 	jfile = f'build/scicrunch/{beg}.json'
 	if os.path.isfile(jfile):
 		beg += delta
 		continue
-	print('downloading', beg)
+	sys.stderr.write(f'downloading scicrunch:{beg}\n')
 	os.system(f'wget --no-check-certificate -q "{url}{beg}" -O {jfile}')
 	beg += delta
 	time.sleep(1)
@@ -42,42 +43,93 @@ while beg <= end:
 ########################
 
 beg = datetime.date(y1, m1, d1)
-papers = {}
-rrids = {}
+papers = {} # indexed by pmid, contains a dict of rrids
 while beg <= end:
 	with open(f'build/scicrunch/{beg}.json') as fp:
-		pubs = json.loads(fp.read())
-	for pub in pubs:
-		if pub['vendor'] == 'MMRRC':
-			rrid = pub['rrid']
-			if rrid not in rrids: rrids[rrid] = 0
-			rrids[pub['rrid']] += 1
-			pmid = pub['pmid']
-			if pmid not in papers:
-				papers[pmid] = {'title': '', 'rrids': []}
-			papers[pmid]['title'] = pub['title'] # there may be duplicates
-			papers[pmid]['rrids'].append(pub['rrid'])
+		sdata = fp.read()
+		if sdata.endswith('not found'):
+			sys.stderr.write(f'record not found: {beg}\n')
+			records = []
+		else:
+			records = json.loads(sdata)
+	for rec in records:
+		if rec['vendor'] == 'MMRRC':
+			pmid = rec['pmid']
+			rrid = rec['rrid']
+			if pmid not in papers: papers[pmid] = {}
+			papers[pmid][rrid] = True
 	beg += delta
+
+##########################
+# MMRRC Download Section #
+##########################
+
+url = 'https://api.mmrrc.org/api/v1/catalog/getCatalogEntry/'
+products = []
+for pub in papers:
+	for rrid in papers[pub]:
+		idx, loc = rrid[11:].split('-')
+		products.append(idx)
+for cat in products:
+	jfile = f'build/mmrrc/{cat}.json'
+	if os.path.isfile(jfile): continue
+	sys.stderr.write(f'downloading mmrrc:{cat}\n')
+	os.system(f'wget --no-check-certificate -q "{url}{cat}" -O {jfile}')
+	time.sleep(1)
+
+###########################
+# PubMed Download Section #
+###########################
+
+url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=' 
+
+for pmid in papers:
+	jfile = f'build/pubmed/{pmid}.json'
+	if os.path.isfile(jfile): continue
+	sys.stderr.write(f'downloading pubmed:{pmid}\n')
+	os.system(f'wget --no-check-certificate -q "{url}{pmid}" -O {jfile}')
+	time.sleep(1)
 
 ##################
 # Output Section #
 ##################
 
+def get_paper(pmid):
+	with open(f'build/pubmed/{pmid}.json') as fp:
+		data = json.loads(fp.read())
+	uid = data['result']['uids'][0]
+	source = data['result'][uid]['source']
+	title = data['result'][uid]['title']
+	return title, source
+
+def get_strain(cat):
+	with open(f'build/mmrrc/{cat}.json') as fp:
+		data = json.loads(fp.read())
+	return data['strain_name']
+
+print('<h1>Publications</h1>')
 pubmed = 'https://pubmed.ncbi.nlm.nih.gov/'
 mmrrc = 'https://www.mmrrc.org/catalog/sds.php?mmrrc_id='
-
-#print('<h1>Publications</h1>')
 for pmid in papers:
-	print(f'<a href="{pubmed}/{pmid}">{pmid}</a> {papers[pmid]["title"]}', end='')
+	title, source = get_paper(pmid)
+	print(f'{title} <em>{source}</em>.', end=' ')
+	print(f'<a href="{pubmed}{pmid}">PMID:{pmid}</a>.', end=' ')
 	txt = []
-	for rrid in papers[pmid]['rrids']:
+	print('RRIDs:', end=' ')
+	for rrid in papers[pmid]:
 		idx, loc = rrid[11:].split('-')
-		txt.append(f' <a href="{mmrrc}{idx}">{rrid[11:]}</a>')
+		txt.append(f'<a href="{mmrrc}{idx}">{rrid[11:]}</a>')
 	print(f'{", ".join(txt)}.<br>')
 
-#print('<h1>Products</h1>')
-#for rrid, val in sorted(rrids.items(), key=lambda item: item[1], reverse=True):
-#	idx, loc = rrid[11:].split('-')
-#	print(f'{idx} ({val}) ')
+print('<h1>Products</h1>')
+rrids = {}
+for pmid in papers:
+	for rrid in papers[pmid]:
+		if rrid not in rrids: rrids[rrid] = 0
+		rrids[rrid] += 1
+for k, v in sorted(rrids.items(), key=lambda item: item[1], reverse=True):
+	idx, loc = k[11:].split('-')
+	print(f'<a href="{mmrrc}{idx}">{k}</a> ({v}) {get_strain(idx)}<br>')
 
 
+sys.stderr.write(f'{len(papers)} papers, {len(rrids)} products\n')
