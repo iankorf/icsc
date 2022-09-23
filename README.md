c167ce9145b9939eb778537dae0825175843dc3b
Ian Korf <ifkorf@ucdavis.edu>
2022-09-23 14:09:08 -0700

    up

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ ICSC Tools
 ==========
 
 pubquarterly.py is to make quaterly reports on publications
+
+## Setup ##
+
+Create directories
+
+	build
+	build/scicrunch
+	build/pubmed
+	build/mmrrc
+
+Run the script. It downloads all the files it needs and creates a report.
+
+	python3 pubquarterly.py 2022-01-01 2022-03-31 > html/2022-Q1.html
--- pubquarterly.py
+++ pubquarterly.py
@@ -28,27 +28,28 @@ delta = datetime.timedelta(days=1)
 # Scicrunch Download Section #
 ##############################
 
-while beg <= end:
-	jfile = f'build/scicrunch/{beg}.json'
+day = beg
+while day <= end:
+	jfile = f'build/scicrunch/{day}.json'
 	if os.path.isfile(jfile):
-		beg += delta
+		day += delta
 		continue
-	sys.stderr.write(f'downloading scicrunch:{beg}\n')
-	os.system(f'wget --no-check-certificate -q "{url}{beg}" -O {jfile}')
-	beg += delta
+	sys.stderr.write(f'downloading scicrunch:{day}\n')
+	os.system(f'wget --no-check-certificate -q "{url}{day}" -O {jfile}')
+	day += delta
 	time.sleep(1)
 
 ########################
 # Organization Section #
 ########################
 
-beg = datetime.date(y1, m1, d1)
+day = beg
 papers = {} # indexed by pmid, contains a dict of rrids
-while beg <= end:
-	with open(f'build/scicrunch/{beg}.json') as fp:
+while day <= end:
+	with open(f'build/scicrunch/{day}.json') as fp:
 		sdata = fp.read()
 		if sdata.endswith('not found'):
-			sys.stderr.write(f'record not found: {beg}\n')
+			sys.stderr.write(f'record not found: {day}\n')
 			records = []
 		else:
 			records = json.loads(sdata)
@@ -58,7 +59,7 @@ while beg <= end:
 			rrid = rec['rrid']
 			if pmid not in papers: papers[pmid] = {}
 			papers[pmid][rrid] = True
-	beg += delta
+	day += delta
 
 ##########################
 # MMRRC Download Section #
@@ -94,37 +95,66 @@ for pmid in papers:
 # Output Section #
 ##################
 
+m2i = {
+	'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6,
+	'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12
+}
+
+def getdate(s):
+	try:
+		y, m, d = s.split(' ')
+		return datetime.date(int(y), m2i[m], int(d))
+	except:
+		return None
+
 def get_paper(pmid):
 	with open(f'build/pubmed/{pmid}.json') as fp:
 		data = json.loads(fp.read())
 	uid = data['result']['uids'][0]
 	source = data['result'][uid]['source']
 	title = data['result'][uid]['title']
-	return title, source
+	pdate = getdate(data['result'][uid]['pubdate'])
+	edate = getdate(data['result'][uid]['epubdate'])
+	if pdate is not None: return title, source, pdate
+	if edate is not None: return title, source, edate
+	return None, None, None
 
 def get_strain(cat):
 	with open(f'build/mmrrc/{cat}.json') as fp:
 		data = json.loads(fp.read())
 	return data['strain_name']
 
+# at some point they started putting in older papers...
+currentpapers = {}
+for pmid in papers:
+	title, source, pdate = get_paper(pmid)
+	if title is None:
+		sys.stderr.write(f'trouble parsing pmid: {pmid}\n')
+		continue
+	if pdate < beg or pdate > end: continue
+	currentpapers[pmid] = papers[pmid]
+
+
 print('<h1>Publications</h1>')
 pubmed = 'https://pubmed.ncbi.nlm.nih.gov/'
 mmrrc = 'https://www.mmrrc.org/catalog/sds.php?mmrrc_id='
-for pmid in papers:
-	title, source = get_paper(pmid)
+
+for pmid in currentpapers:
+	title, source, pdate = get_paper(pmid)
+	
 	print(f'{title} <em>{source}</em>.', end=' ')
 	print(f'<a href="{pubmed}{pmid}">PMID:{pmid}</a>.', end=' ')
 	txt = []
 	print('RRIDs:', end=' ')
-	for rrid in papers[pmid]:
+	for rrid in currentpapers[pmid]:
 		idx, loc = rrid[11:].split('-')
 		txt.append(f'<a href="{mmrrc}{idx}">{rrid[11:]}</a>')
 	print(f'{", ".join(txt)}.<br>')
 
 print('<h1>Products</h1>')
 rrids = {}
-for pmid in papers:
-	for rrid in papers[pmid]:
+for pmid in currentpapers:
+	for rrid in currentpapers[pmid]:
 		if rrid not in rrids: rrids[rrid] = 0
 		rrids[rrid] += 1
 for k, v in sorted(rrids.items(), key=lambda item: item[1], reverse=True):
@@ -132,4 +162,4 @@ for k, v in sorted(rrids.items(), key=lambda item: item[1], reverse=True):
 	print(f'<a href="{mmrrc}{idx}">{k}</a> ({v}) {get_strain(idx)}<br>')
 
 
-sys.stderr.write(f'{len(papers)} papers, {len(rrids)} products\n')
+sys.stderr.write(f'{len(currentpapers)} papers, {len(rrids)} products\n')
